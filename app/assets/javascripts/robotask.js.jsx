88ab8d663c5c35dadf9d36e439dc4e841bb85c25
Florante Dela Cruz <fndelacruz@gmail.com>
2015-10-13 15:25:23 -0700

    add conditional to render React root so redirect works properly if not logged in

--- app/assets/javascripts/robotask.js.jsx
+++ app/assets/javascripts/robotask.js.jsx
@@ -1,30 +1,30 @@
 $(function() {
   'use strict';
+  if (window.CURRENT_USER_ID) {
+    var root = document.getElementById("static-root");
+    var Router = ReactRouter.Router;
+    var Route = ReactRouter.Route;
+    var IndexRoute = ReactRouter.IndexRoute;
 
-  var root = document.getElementById("static-root");
-  var Router = ReactRouter.Router;
-  var Route = ReactRouter.Route;
-  var IndexRoute = ReactRouter.IndexRoute;
+    var App = root.App = React.createClass({
+     render: function() {
+       return (
+         <div>
+          <header>
+            <h3>App Component Placeholder</h3>
+          </header>
+          {this.props.children}
+         </div>
+       );
+     }
+    });
 
-  var App = root.App = React.createClass({
-   render: function() {
-     return (
-       <div>
-        <header>
-          <h3>App Component Placeholder</h3>
-        </header>
-        {this.props.children}
-       </div>
-     );
-   }
-  });
+    var routes = (
+      <Route path="/" component={App}>
+        <IndexRoute component={NavBar} />
+      </Route>
+    );
 
-  var routes = (
-    <Route path="/" component={App}>
-      <IndexRoute component={NavBar} />
-    </Route>
-  );
-
-  React.render(<Router>{routes}</Router>, root);
-
+    React.render(<Router>{routes}</Router>, root);
+  }
 });
