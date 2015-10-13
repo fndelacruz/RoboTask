310db606457bcc7aef1fa1eb594fab4e5a24df50
Florante Dela Cruz <fndelacruz@gmail.com>
2015-10-13 11:18:34 -0700

    add seed data to test user/task assocations. works fine

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([
+  {
+    email: "user1",
+    password_digest: BCrypt::Password.create("password"),
+    bio: "user1 bio"
+  }
+])
+newest_user_id = User.maximum(:id)
+
+Task.create([
+  {
+    title: "task1 title",
+    description: "task1 description",
+    location: "task1 location",
+    creator_id: newest_user_id
+  },
+  {
+    title: "task2 title",
+    description: "task2 description",
+    location: "task2 location",
+    creator_id: newest_user_id
+  }
+])
+
+User.create([
+  {
+    email: "user2",
+    password_digest: BCrypt::Password.create("password"),
+    bio: "user2 bio"
+  }
+])
+newest_user_id = User.maximum(:id)
+
+
+Task.create([
+  {
+    title: "task3 title",
+    description: "task3 description",
+    location: "task3 location",
+    creator_id: newest_user_id
+  },
+])
+
+User.create([
+  {
+    email: "user3",
+    password_digest: BCrypt::Password.create("password"),
+    bio: "user3 bio"
+  }
+])
