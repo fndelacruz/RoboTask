cde4435d5c581b32878dba8eda73f2890ac162d0
Florante Dela Cruz <fndelacruz@gmail.com>
2015-10-13 10:47:55 -0700

    changed App name to RoboTask. created root to session#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'session#new'
   resources :users, only: [:new, :create, :show]
   resources :session, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
