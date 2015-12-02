fd9e7ac6c176540a46c0be27a2834bfbec029822
Florante Dela Cruz <fndelacruz@gmail.com>
2015-12-01 22:10:13 -0800

    testing adsense

---
+++ app/assets/javascripts/components/adsense.js.jsx
@@ -0,0 +1,17 @@
+// (function(root) {
+//   'use strict';
+//
+//   root.Adsense = React.createClass({
+//     render: function() {
+//       return (
+//         <div
+//           className="adsbygoogle"
+//           style="display:block"
+//           data-ad-client="ca-pub-1031548899373280"
+//           data-ad-slot="7095306654"
+//           data-ad-format="auto"
+//         />
+//       );
+//     }
+//   });
+// }(this));
